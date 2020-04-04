06c7483aeff6b7f131a35106be14e7045264d1de
arun jacob <arun.jacobpackianathan@cognizant.com>
2020-04-04 13:10:56 +0530

    ui code added

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,6 +31,14 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod()
+                    .WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -46,7 +54,7 @@ namespace API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //app.UseHsts();
             }
-
+            app.UseCors("CorsPolicy");
             //app.UseHttpsRedirection();
             app.UseMvc();
         }
